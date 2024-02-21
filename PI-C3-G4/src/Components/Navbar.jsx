b7bdd937b1521a9a3364ac1b0fbf0e9dce8744c3
Matuterete <EscalanteMatias22@gmail.com>
2024-02-21 02:17:29 -0300

    header responsive

--- PI-C3-G4/src/Components/Navbar.jsx
+++ PI-C3-G4/src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Sun from '../assets/Sun.svg';
 import Moon from '../assets/Moon.svg';
 import Logo from '../assets/Logo.png';
@@ -7,9 +7,9 @@ import { useContext } from '../Utils/Context.jsx';
 import { TOGGLE_THEME } from '../Reducers/Reducer.jsx';
 import './styles/Navbar.css';
 
-
 const Navbar = () => {
   const { state, dispatch } = useContext();
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   const routes = [
     { path: '/home', name: 'Home' },
@@ -19,26 +19,34 @@ const Navbar = () => {
   const handleTheme = () => {
     dispatch({ type: TOGGLE_THEME });
   }
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  }
+
   return (
     <section className='header'>
       <nav className='navbar'>
         <div className='navbar-logo'>
           <Link to={routes[0].path}>
-            <img src={Logo} width='308px'></img>
+            <img src={Logo} alt="Logo"></img>
           </Link>
         </div>
-        <ul>
-          {routes.map((route, index) => (
-            <li key={index}>
-              <Link to={route.path}>{route.name}</Link>
-            </li>
-          ))}
-        </ul>
+        
+      <nav id='mobile'>
+        <button id="menu-toggle" onClick={toggleMobileMenu}>☰</button>
+        <div id="mobile-menu" className={isMobileMenuOpen ? 'active' : ''}>
+        <div className="mobile-menu-items">
+              {routes.map((route, index) => (<a key={index} href={route.path}>{route.name}</a>))}
+        </div>
+        </div>
+      </nav>
+
         <div className='buttons'>
           <button className='btn-login'>Iniciar Sesión</button>
           <button className='btn-registro'>Registrarse</button>
           <button className='btn-theme' onClick={handleTheme}>
-            <img src={state.theme === 'light' ? Moon : Sun} width='25px'></img>
+            <img src={state.theme === 'light' ? Moon : Sun} width='25px' alt="Theme"></img>
           </button>
         </div>
       </nav>
