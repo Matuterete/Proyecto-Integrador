afa856822f98b63eccfcb93e300858ba5af82a64
Martin-Cerbin <martinpcerbin@gmail.com>
2024-03-04 20:43:16 -0300

    borrado de array products del front

--- PI-C3-G4/src/Routes/Home.jsx
+++ PI-C3-G4/src/Routes/Home.jsx
@@ -5,58 +5,8 @@ import { useContext } from '../Utils/Context';
 import requestToAPI from '../services/requestToAPI';
 
 const Home = () => {
-  // ARRAY PARA CATEGORIAS
-  const [categories, setCategories] = useState([
-    {
-      id: 1,
-      name: 'Eventos grandes',
-      description: 'Descripción de eventos grandes',
-      photo: 'https://via.placeholder.com/150'
-    },
-    {
-      id: 2,
-      name: 'Eventos medianos',
-      description: 'Descripción de eventos medianos',
-      photo: 'https://via.placeholder.com/150'
-    },
-    {
-      id: 3,
-      name: 'Eventos pequeños',
-      description: 'Descripción de eventos pequeños',
-      photo: 'https://via.placeholder.com/150'
-    }
-  ]);
 
-  const { state } = useContext()
-
-  const products = state.data
-
-  const [productsRandom, setProductsRandom] = useState([]);
-
-  useEffect(() => {
-    // Generar 4 índices aleatorios únicos ¡¡esto lo debe generar la api!!
-    const indicesAleatorios = [];
-    while (indicesAleatorios.length < 6) {
-      const indiceAleatorio = Math.floor(Math.random() * products.length);
-      if (!indicesAleatorios.includes(indiceAleatorio)) {
-        indicesAleatorios.push(indiceAleatorio);
-      }
-    }
-
-    // Mostrar los objetos correspondientes a los índices seleccionados
-    const productsSeleccionados = indicesAleatorios.map(indice => products[indice]);
-    setProductsRandom(productsSeleccionados);
-  }, []);
-  /*
-    <div className='Categories'>
-      <p className='cardTitle-1'>Categorias</p>
-      <div className='cardGrid-1'>
-        {categories.map(product => <Card product={product} key={product.id} />)}
-      </div>
-    </div>
-  */
   const [responseData, setResponseData] = useState()
-
   useEffect(() => {
     async function fetchData() {
       try {
