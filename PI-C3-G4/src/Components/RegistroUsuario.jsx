01274be536b2fc5a20822ae8b0aafc8e18740420
digpolo <digpolo@gmail.com>
2024-03-14 16:20:43 -0500

    add timer

--- PI-C3-G4/src/Components/RegistroUsuario.jsx
+++ PI-C3-G4/src/Components/RegistroUsuario.jsx
@@ -7,6 +7,7 @@ import registroUsuario from '../assets/imagen-Usuario.png';
 import EmailConfirmation from '../Components/EmailConfirmation'
 
 import emailjs from '@emailjs/browser';
+import Timer from './Timer';
 
 
 function RegistroUsuario() {
@@ -15,7 +16,8 @@ function RegistroUsuario() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [password2, setPassword2] = useState('');
-  const [mostrarDespuesDeGuardar, setMostrarDespuesDeGuardar] = useState(false)
+  const [mostrarDespuesDeGuardar, setMostrarDespuesDeGuardar] = useState(true)
+  const [formData, setFormData] = useState({})
 
   const handleNameChange = (e) => {
     setName(e.target.value);
@@ -48,11 +50,13 @@ function RegistroUsuario() {
   };
 
   const form = useRef();
-  
+
   const enviarCorreo = () => {
+
     emailjs
-      .sendForm('service_9nydvzd', 'template_vf09e5z', form.current, {
-        publicKey: '_tQw4BNfAWBkcyhJO',
+      .sendForm('service_ip36kkm', 'template_npdyv08', form.current, {
+        publicKey: 'mTXpE_At67OgrjMJD',
+        ...formData
       })
       .then(
         () => {
@@ -66,8 +70,9 @@ function RegistroUsuario() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    enviarCorreo();
 
+    const formData = { name, lastName, email, password };
+    setFormData(formData);
 
     if (password == password2) {
       // Aquí podrías enviar los datos a un servidor o hacer cualquier otra acción con ellos
@@ -79,21 +84,22 @@ function RegistroUsuario() {
       //   password
       // })
       //   .then(response => {
-          // alert(`El usuario ${response.data.name} ha quedado registrado correctamente.`)
-          setMostrarDespuesDeGuardar(true);
-        // })
-    //     .catch(error => {
-    //       console.error(error);
-    //       alert('Hubo un error al intentar registrar el usuario');
-    //     });
-    // }
-    // else {
-    //   alert('Las contraseñas no coinciden');
-    // }
+      // alert(`El usuario ${response.data.name} ha quedado registrado correctamente.`)
+      setMostrarDespuesDeGuardar(false);
+      enviarCorreo();
+      // })
+      //     .catch(error => {
+      //       console.error(error);
+      //       alert('Hubo un error al intentar registrar el usuario');
+      //     });
+      // }
+      // else {
+      //   alert('Las contraseñas no coinciden');
+      // }
 
-    //  navigate("/emailRegister");
-  };
-}
+      //  navigate("/emailRegister");
+    };
+  }
 
 
 
@@ -103,48 +109,49 @@ function RegistroUsuario() {
       <div className='container-img'>
         <img src={registroUsuario} alt="logo registro de usuario" height="100px" width="100px" />
       </div>
-      {mostrarDespuesDeGuardar ? (
-    
-      <div> 
-           <h1>Confirma tu correo</h1>
-        <p>Hemos enviado un mensaje de confirmación a tu dirección de correo electrónico.</p>
-        <p>Por favor, revisa tu bandeja de entrada para confirmar tu registro. Si no encuentras el mensaje de confirmación en tu bandeja de entrada, te sugerimos revisar en la carpeta de correo no deseado (spam).</p>
-       
-        <button type="submit" className='button buttonSecundary' onClick={ enviarCorreo}>Reenviar correo</button>
-        </div>     
-      ) : (
+
+      <form ref={form} onSubmit={handleSubmit}>
 
 
-        <form ref={form} onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label>Nombre:
-              <input type="text" name="name" value={name} onChange={handleNameChange} />
-            </label>
-          </div>
-          <div className="form-group">
-            <label>Apellido:
-              <input type="text" name="lastName" value={lastName} onChange={handleLastNameChange} />
-            </label>
-          </div>
-          <div className="form-group">
-            <label>Correo electrónico:
-              <input type="email" name="email" value={email} onChange={handleEmailChange} />
-            </label>
-          </div>
-          <div className="form-group">
-            <label>Contraseña:
-              <input type="password" name="password" value={password} onChange={handlePasswordChange} />
-            </label>
-          </div>
-          <div className="form-group">
-            <label>Repita contraseña:
-              <input type="password" name="password2" value={password2} onChange={handlePassword2Change} />
-            </label>
-          </div>
-          <div className="boton"><button className='button buttonSecundary' type="submit">Guardar Usuario</button></div>
-        </form>
+        <div className="form-group">
+          <label>Nombre:
+            <input type="text" name="name" value={name} onChange={handleNameChange} />
+          </label>
+        </div>
+        <div className="form-group">
+          <label>Apellido:
+            <input type="text" name="lastName" value={lastName} onChange={handleLastNameChange} />
+          </label>
+        </div>
+        <div className="form-group">
+          <label>Correo electrónico:
+            <input type="email" name="email" value={email} onChange={handleEmailChange} />
+          </label>
+        </div>
+        <div className="form-group">
+          <label>Contraseña:
+            <input type="password" name="password" value={password} onChange={handlePasswordChange} />
+          </label>
+        </div>
+        <div className="form-group">
+          <label>Repita contraseña:
+            <input type="password" name="password2" value={password2} onChange={handlePassword2Change} />
+          </label>
+        </div>
+        <div className="boton"><button className='button buttonSecundary' type="submit">Guardar Usuario</button></div>
 
-      )}
+        {mostrarDespuesDeGuardar ? (<div></div>
+        ) : (
+          <div>
+            <h1>Confirma tu correo</h1>
+            <p>Hemos enviado un mensaje de confirmación a tu dirección de correo electrónico.</p>
+            <Timer />
+            <button type="submit" className='button buttonSecundary'>Reenviar correo</button>
+          </div>
+        )}
+      </form>
+
+
 
     </div>
   );
@@ -154,4 +161,16 @@ function RegistroUsuario() {
 export default RegistroUsuario;
 
 
-  //  <EmailConfirmation emailform={email} nameform={name} handleSubmit={handleSubmit} />
+{/* <div>
+          <h1>Confirma tu correo</h1>
+          <p>Hemos enviado un mensaje de confirmación a tu dirección de correo electrónico.</p>
+          <p>Por favor, revisa tu bandeja de entrada para confirmar tu registro. Si no encuentras el mensaje de confirmación en tu bandeja de entrada, te sugerimos revisar en la carpeta de correo no deseado (spam).</p>
+          <form ref={form} onSubmit={enviarCorreo}>
+            <input type="text" name="name" defaultValue={formData.name} style={{ display: 'none' }} />
+            <input type="email" name="email" defaultValue={formData.email} style={{ display: 'none' }} />
+            <button type="submit" className='button buttonSecundary'>Reenviar correo</button>
+          </form>
+        
+        </div> */}
+
+// //  <EmailConfirmation emailform={email} nameform={name} handleSubmit={handleSubmit} />
