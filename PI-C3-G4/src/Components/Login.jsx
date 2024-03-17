4019556e1c56b262125ca4964ccb4216b03eff7f
camilaramirez10 <camilaram2009@hotmail.com>
2024-03-17 00:11:34 -0500

    Se quita codigo innecesario

--- PI-C3-G4/src/Components/Login.jsx
+++ PI-C3-G4/src/Components/Login.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import '../Components/styles/Login.css';
 
 function Login() {
   const [usuario, setUsuario] = useState({
