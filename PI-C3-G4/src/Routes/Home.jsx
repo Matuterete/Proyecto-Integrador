5ce1e10c8aff68e8f33ae430032ba415744448f5
Matuterete <EscalanteMatias22@gmail.com>
2024-03-19 02:58:23 -0300

    categorias del home actualizadas. Falta mejorar mínimamente

--- PI-C3-G4/src/Routes/Home.jsx
+++ PI-C3-G4/src/Routes/Home.jsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Card from '../Components/Card';
 import requestToAPI from '../services/requestToAPI';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
 import '../Components/styles/Home.css';
 
 const Home = () => {
@@ -9,6 +13,7 @@ const Home = () => {
   const [categoriaSeleccionada, setCategoriaSeleccionada] = useState('');
   const [mostrarProductosPorCategoria, setMostrarProductosPorCategoria] = useState(false);
   const [productosRecomendados, setProductosRecomendados] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function fetchData() {
@@ -38,17 +43,62 @@ const Home = () => {
     if (categoriaSeleccionada === categoryTitle) {
       setCategoriaSeleccionada('');
       setMostrarProductosPorCategoria(false);
+      setProductosPorCategoria([]);
     } else {
       try {
         setCategoriaSeleccionada(categoryTitle);
         const response = await requestToAPI(`http://prothechnics.us.to:8080/products/find/category/${categoryId}`, 'GET');
-        setProductosPorCategoria(response);
-        setMostrarProductosPorCategoria(true);
+        console.log("Respuesta de la API:", response); // Agrega este console.log
+        if (Array.isArray(response)) {
+          setProductosPorCategoria(response);
+          setMostrarProductosPorCategoria(true);
+        } else {
+          setProductosPorCategoria([response]); // Convertir a un array de un solo elemento
+          setMostrarProductosPorCategoria(true);
+        }
       } catch (error) {
         console.error('Error fetching products by category:', error);
       }
     }
   };
+
+  const handleProductoClick = (product) => {
+    navigate(`/detail/${product.id}`, { state: { product } });
+  };
+
+  const [sliderSettings, setSliderSettings] = useState({
+    dots: true,
+    infinite: false, 
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 768) {
+        setSliderSettings({
+          ...sliderSettings,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        });
+      } else {
+        const slidesToShow = productosPorCategoria.length > 1 ? 4 : 1;
+        setSliderSettings({
+          ...sliderSettings,
+          slidesToShow: slidesToShow,
+          slidesToScroll: 1 
+        });
+      }
+    };
+  
+    handleResize();
+    window.addEventListener('resize', handleResize);
+  
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [productosPorCategoria.length]);
 
   return (
     <div className='body'>
@@ -71,15 +121,18 @@ const Home = () => {
       {mostrarProductosPorCategoria && (
         <div className="productos-por-categoria">
           <h1>{categoriaSeleccionada}</h1>
-          <div className="productos-container">
-            {productosPorCategoria.map(producto => (
-              <div key={producto.id} className="producto">
-                <p>{producto.name}</p>
-                <p>{producto.description}</p>
-                <p>Precio: {producto.price}</p>
-              </div>
-            ))}
-          </div>
+          <Slider {...sliderSettings}>
+            {productosPorCategoria.map(producto => {
+              console.log("Productos por categoría:", productosPorCategoria); 
+              return (
+                <div key={producto.id} className="producto" onClick={() => handleProductoClick(producto)}>
+                  <div className="card-wrapper">
+                    <Card product={producto} />
+                  </div>
+                </div>
+              );
+            })}
+          </Slider>
         </div>
       )}
 
