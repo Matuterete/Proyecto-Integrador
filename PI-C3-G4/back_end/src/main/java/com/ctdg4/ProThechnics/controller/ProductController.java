8e17cca28ee8a290724c1766b4395fcf941f77f7
Kike Gómez <kike.gomez.uy@gmail.com>
2024-03-21 19:02:48 -0300

    Ajustes Alta Producto y Detail

--- PI-C3-G4/back_end/src/main/java/com/ctdg4/ProThechnics/controller/ProductController.java
+++ PI-C3-G4/back_end/src/main/java/com/ctdg4/ProThechnics/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.ctdg4.ProThechnics.entity.Product;
 import com.ctdg4.ProThechnics.entity.ProductImage;
 import com.ctdg4.ProThechnics.exception.DuplicateException;
 import com.ctdg4.ProThechnics.exception.ResourceNotFoundException;
+import com.ctdg4.ProThechnics.service.ProductImageService;
 import com.ctdg4.ProThechnics.service.ProductService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -28,6 +29,7 @@ import java.util.stream.Collectors;
 public class ProductController {
 
     private ProductService productService;
+    private ProductImageService productImageService;
     private Set<Long> lastReturnedProductIds = new HashSet<>();
 
     @Autowired
@@ -49,6 +51,17 @@ public class ProductController {
         }
         return ResponseEntity.ok(productService.saveProduct(product));
     }
+
+//    @Operation(summary = "Register a new product image", description = "Registers a new product image in the system")
+//    @ApiResponse(responseCode = "200", description = "Product image registered successfully",
+//            content = @Content(schema = @Schema(implementation = ProductImage.class)))
+//    @PostMapping("/images/add")
+//    public ResponseEntity<List<ProductImage>> registerProductImages(@RequestBody List<ProductImage> productImages) {
+//        List<ProductImage> savedImages = productImages.stream()
+//                .map(productImageService::saveProductImage)
+//                .collect(Collectors.toList());
+//        return ResponseEntity.ok(savedImages);
+//    }
 
     @Operation(summary = "Find all products", description = "Retrieves a list of all products")
     @ApiResponse(responseCode = "200", description = "List of products",
--- PI-C3-G4/src/Components/ProductAddForm.jsx
+++ PI-C3-G4/src/Components/ProductAddForm.jsx
@@ -1,19 +1,19 @@
 import { useState, useEffect } from "react";
 import Swal from "sweetalert2";
 import requestToAPI from "../services/requestToAPI";
-import axios from "axios";
 
 function ProductAddForm({ onAdd, onCancel }) {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
-  const [price, setPrice] = useState("");
+  const [price, setPrice] = useState(0);
   const [isActive, setIsActive] = useState(true);
-  const [stock, setStock] = useState("0");
+  const [stock, setStock] = useState(1);
   const [categories, setCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState("");
+  const [features, setFeatures] = useState([]);
+  const [selectedFeature, setSelectedFeature] = useState("");
   const [primaryImage, setPrimaryImage] = useState(null);
   const [additionalImages, setAdditionalImages] = useState([]);
-  const [imageUrls, setImageUrls] = useState([]);
 
   useEffect(() => {
     requestToAPI("categories/find/all", "GET")
@@ -24,6 +24,18 @@ function ProductAddForm({ onAdd, onCancel }) {
         console.error(error);
       });
   }, []);
+
+  useEffect(() => {
+    requestToAPI("features/find/all", "GET")
+      .then((response) => {
+        setFeatures(response);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  // Handlers
 
   const handleNameChange = (e) => {
     setName(e.target.value);
@@ -49,33 +61,72 @@ function ProductAddForm({ onAdd, onCancel }) {
     setSelectedCategory(e.target.value);
   };
 
+  const handleFeatureChange = (e) => {
+    setSelectedFeature(e.target.value);
+  };
+
   const handlePrimaryImageChange = (e) => {
     setPrimaryImage(e.target.files[0]);
   };
 
   const handleAdditionalImagesChange = (e) => {
     const files = Array.from(e.target.files);
-    const updatedImages = {};
-    
-    files.forEach((file, index) => {
-      updatedImages[index] = file;
-    });
-  
-    setAdditionalImages(updatedImages);
+    setAdditionalImages(files);
   };
 
-  // Función para renombrar las imágenes
+  // Funciones
+
   const renameImage = (image, productId, imageNumber) => {
-    const extension = image.name.split(".").pop(); 
+    const extension = image.name.split(".").pop();
     const newName = `id_${productId}_img_${imageNumber}.${extension}`;
     return new File([image], newName, { type: image.type });
   };
 
+  const buildImageData = (
+    primaryImageUrl,
+    additionalImageUrls,
+    productName,
+    productId
+  ) => {
+    const imageData = [];
+
+    const primaryImage = {
+      title: `${productName} 1`,
+      url: primaryImageUrl,
+      isPrimary: true,
+      product: {
+        id: productId,
+      },
+    };
+
+    imageData.push(primaryImage);
+
+    if (additionalImageUrls.length > 0) {
+      additionalImageUrls.forEach((imageUrl, index) => {
+        const additionalImage = {
+          title: `${productName} ${index + 2}`,
+          url: imageUrl,
+          isPrimary: false,
+          product: {
+            id: productId,
+          },
+        };
+        imageData.push(additionalImage);
+      });
+    }
+
+    return imageData;
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
+    let productId;
+    let bucketS3Response;
+
     try {
-      const productResponse = await axios.post("http://localhost:8080/api/products/add", {
+      //1 - Se guarda primero el producto
+      const productResponse = await requestToAPI("products/add", "POST", {
         name,
         description,
         price,
@@ -85,20 +136,69 @@ function ProductAddForm({ onAdd, onCancel }) {
           id: selectedCategory,
         },
       });
-  
-      const productId = productResponse.data.id;
+
+      // 2 - Se guardan las imagenes en Bucket S3 AWS
+      productId = productResponse.id;
 
       const formData = new FormData();
       formData.append("files", renameImage(primaryImage, productId, 1));
-  
+
       for (let i = 0; i < additionalImages.length; i++) {
-        formData.append("files", renameImage(additionalImages[i], productId, i + 2));
+        formData.append(
+          "files",
+          renameImage(additionalImages[i], productId, i + 2)
+        );
       }
 
-      await axios.post(
-        `http://localhost:8080/api/storage/products/uploadFiles`,
+      const bucketS3Response = await requestToAPI(
+        "storage/products/uploadFiles",
+        "POST",
         formData
       );
+
+      // 3 - Se asocian las URLs de S3 a el producto en la Base de Datos
+
+      const imageUrls = bucketS3Response;
+      console.log(imageUrls);
+      let primaryImageUrl = "";
+      let additionalImageUrls = [];
+
+      if (imageUrls.length === 1) {
+        primaryImageUrl = imageUrls[0];
+      } else if (imageUrls.length > 1) {
+        primaryImageUrl = imageUrls[0];
+        additionalImageUrls = imageUrls.slice(1);
+      }
+
+      const productName = name;
+
+      const imageData = buildImageData(
+        primaryImageUrl,
+        additionalImageUrls,
+        productName,
+        productId
+      );
+
+      let endpoint;
+      if (primaryImageUrl.length === 1) {
+        endpoint = "products/images/add";
+      } else {
+        endpoint = "products/images/multiple/add";
+      }
+
+      await requestToAPI(endpoint, "POST", imageData);
+
+      // Se limpia el Formulario si todo salio bien
+      setName("");
+      setDescription("");
+      setPrice(0);
+      setIsActive(true);
+      setStock(1);
+      setSelectedCategory("");
+      setPrimaryImage(null);
+      setAdditionalImages([]);
+      document.getElementById("primaryImageInput").value = null;
+      document.getElementById("additionalImagesInput").value = null;
 
       Swal.fire({
         icon: "success",
@@ -106,15 +206,47 @@ function ProductAddForm({ onAdd, onCancel }) {
         showConfirmButton: false,
         timer: 2000,
       });
-
     } catch (error) {
-      Swal.fire({
-        icon: "error",
-        title: "Error inesperado",
-        text: "Hubo un error al intentar agregar el producto",
-        showConfirmButton: true,
-      });
-      console.error(error);
+      console.error("Error al crear el producto:", error);
+      if (error.response && error.response.status === 409) {
+        Swal.fire({
+          icon: "error",
+          title: "Error de duplicidad",
+          text: "El producto ingresado ya existe",
+          showConfirmButton: true,
+        });
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Error inesperado",
+          text: "Hubo un error al intentar agregar el producto",
+          showConfirmButton: true,
+        });
+
+        if (productId) {
+          try {
+            await requestToAPI(`products/delete/id/${productId}`, "DELETE");
+          } catch (deleteError) {
+            console.error("Error al borrar el producto:", deleteError);
+          }
+        }
+
+        if (bucketS3Response) {
+          const deleteFormData = new FormData();
+          for (const url of bucketS3Response.data) {
+            deleteFormData.append("url", url);
+          }
+
+          try {
+            await requestToAPI("storage/deleteFile", "POST", deleteFormData);
+          } catch (deleteFileError) {
+            console.error(
+              "Error al borrar los archivos de S3:",
+              deleteFileError
+            );
+          }
+        }
+      }
     }
   };
 
@@ -188,27 +320,53 @@ function ProductAddForm({ onAdd, onCancel }) {
           </select>
         </label>
       </div>
-      <div className="form-group">
-        <label>
-          Imagen Principal:
-          <input
-            type="file"
-            accept="image/*"
-            onChange={handlePrimaryImageChange}
-            required
-          />
-        </label>
+      <div>
+        <p>Imágenes (1MB Max)</p>
+        <div className="form-group">
+          <label>
+            Portada del Producto:
+            <input
+              type="file"
+              accept="image/*"
+              id="primaryImageInput"
+              onChange={handlePrimaryImageChange}
+              required
+            />
+          </label>
+        </div>
+        <div className="form-group">
+          <label>
+            Adicionales:
+            <input
+              type="file"
+              accept="image/*"
+              multiple
+              id="additionalImagesInput"
+              onChange={handleAdditionalImagesChange}
+            />
+          </label>
+        </div>
       </div>
-      <div className="form-group">
-        <label>
-          Imágenes Adicionales:
-          <input
-            type="file"
-            accept="image/*"
-            multiple
-            onChange={handleAdditionalImagesChange}
-          />
-        </label>
+      <div className="form-group feature">
+        <div className="form-group">
+          <label>
+            Características:
+            <select value={selectedFeature} onChange={handleFeatureChange}>
+              <option value="">Selecciona una característica</option>
+              {features.map((feature) => (
+                <option key={feature.id} value={feature.id}>
+                  {feature.title}
+                </option>
+              ))}
+            </select>
+          </label>
+        </div>
+        <div className="form-group">
+          <label>
+            Valor:
+            <input type="text" />
+          </label>
+        </div>
       </div>
       <div className="form-group buttonCenter">
         <button type="submit" className="button buttonPrimary buttonBig">
