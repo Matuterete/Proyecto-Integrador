80c12f34f138f935038a1fd070bca6771a825ac4
Kike Gómez <kike.gomez.uy@gmail.com>
2024-04-05 04:03:09 -0300

    Ajuste credenciales SMTP

--- PI-C3-G4/back_end/src/main/java/com/ctdg4/ProThechnics/service/MailService.java
+++ PI-C3-G4/back_end/src/main/java/com/ctdg4/ProThechnics/service/MailService.java
@@ -5,13 +5,16 @@ import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MailService {
+
+    @Autowired
+    private Environment env;
     @Autowired
     private JavaMailSender mailSender;
     @Value("${spring.mail.username}")
